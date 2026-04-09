46c051ae8681e5c308208cdc3143c37c247dab33
phua <phuawenpu@users.noreply.github.com>
2026-04-09 16:59:06 +0800

    PWA: encrypted URL, rate limiting, collapsible keyboard, fix dart:io on web

---
+++ backend/agent/wasm/ktty_wasm_crypto_bg.wasm.d.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const computeHmac: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const decrypt: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const deriveKey: (a: number, b: number) => [number, number, number, number];
+export const encrypt: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const mlkemDecapsulate: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const mlkemEncapsulate: (a: number, b: number) => [number, number, number, number];
+export const mlkemGenerateKeypair: () => [number, number];
+export const roomId: (a: number, b: number) => [number, number, number, number];
+export const verifyHmac: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_start: () => void;
